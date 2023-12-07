7dd28af475d49773a644f24230464d4cf48083f2
shwetambari <shwetambari.jadhav@capgemini.com>
2023-12-07 14:42:01 +0000

    initial commit

---
+++ src/test/java/com/example/demoPipeline/DemoPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoPipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
